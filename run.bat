3ed8afbaf1a8011157a446fe5db8d2efc1fe3a96
bicantester <bicantester@gmail.com>
2024-05-14 14:33:00 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 if not exist "%homedrive%%homepath%\tdt_datasets" mkdir "%homedrive%%homepath%\tdt_datasets"
 
-docker run -v "%cd%:/work" -v "%homedrive%%homepath%\tdt_datasets:/tdt_datasets" -w /work --rm -ti -p 3000:3000 -p 8000:8000 -p 5000:5000 -e ghcr.io/brain-bican/taxonomy-development-tools:tester %*
+docker run -v "%cd%:/work" -v "%homedrive%%homepath%\tdt_datasets:/tdt_datasets" -w /work --rm -ti -p 3000:3000 -p 8000:8000 -p 5000:5000 ghcr.io/brain-bican/taxonomy-development-tools:tester %*
